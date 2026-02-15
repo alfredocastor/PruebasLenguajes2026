ee2be891706aa94b65f0b1e1d55f246ba8cc8db4
Edwin Omar Guerrero Godina <omar.09edwin@gmail.com>
2026-02-15 12:49:17 -0600

    prueba
    hice una prueba para identificadores

---
+++ Identificadores.java
@@ -0,0 +1,11 @@
+public class Identificadores {
+    public static void main(String[] args) {
+        int numero = 10;
+        String nombre = "Juan";
+        double precio = 19.99;
+
+        System.out.println("Número: " + numero);
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Precio: " + precio);
+    }
+}
