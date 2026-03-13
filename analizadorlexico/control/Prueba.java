1588b4e630ec20f12a55daf71cf04918194c4f07
Edwin Omar Guerrero Godina <omar.09edwin@gmail.com>
2026-03-13 08:27:06 -0600

    ya no le mevas perro

--- analizadorlexico/control/Prueba.java
+++ analizadorlexico/control/Prueba.java
@@ -3,10 +3,10 @@ package analizadorlexico.control;
 public class Prueba {
     public static void main(String[] args) {
         System.out.println("Prueba de la clase Lexemas:");
-        System.out.println("Token para Identificador: " + Lexemas.Tokens.IDENTIFICADOR);
-        System.out.println("Token para Número: " + Lexemas.Tokens.NUMERO);
-        System.out.println("Token para Igualdad (==): " + Lexemas.Tokens.IGUALDAD);
-        System.out.println("Token para Diferencia (!=): " + Lexemas.Tokens.DIFERENCIA);
-        System.out.println("Token para Menor que (<): " + Lexemas.Tokens.MENOR_QUE);
+        System.out.println("Token para Identificador: " + Lexemas.IDENTIFICADOR);
+        System.out.println("Token para Número: " + Lexemas.NUMERO);
+        System.out.println("Token para Igualdad (==): " + Lexemas.IGUALDAD);
+        System.out.println("Token para Diferencia (!=): " + Lexemas.DIFERENCIA);
+        System.out.println("Token para Menor que (<): " + Lexemas.MENOR_QUE);
     }
 }
